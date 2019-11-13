90cc5113ee18000ff5e9577ddbdb486cfa1f4344
David Mendez <davywavy@Davids-MacBook-Pro-3.local>
2019-11-13 14:26:02 -0600

    first commit

---
+++ reactportfolio/ClientApp/src/resumeData.js
@@ -0,0 +1,85 @@
+let resumeData = {
+    "name": "David Mendez",
+    "role": "Cyber Security Specialist",
+    "email":"davidmendez@lewisu.edu",
+    "phonenum": "(815) 514-1195",
+    "roleDescription": "I am looking for a challenging and rewarding role in the cyber security field",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/david-mendez-43394114b/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/dmendez1/projects",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"Currently I am finishing up my senior year at Lewis University, I will be receiving my B.S. in Computer Science with a concentration in Cyber Security. ",
+    "address":"Chicago, IL",
+    "education":[
+      {
+        "UniversityName":"Lewis University",
+        "specialization":"Computer Science with a Concentration in Cyber Security.",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2020",
+
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Rubicon Group Ltd.",
+        "specialization":"Software Developer Intern",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Php Software Development"
+      },
+      {
+        "CompanyName":"Chicago POS Systems",
+        "specialization":"IT Specialist",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2019",
+        "Achievements":"Networking with overall technical support"
+      }
+    ],
+    "skillsDescription":"Skills",
+    "skill1":"Python",
+    "skill2":"React Js",
+    "skill3":"HTML5/CSS3",
+    "skills":[
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Java",
+        "description":"Java Programs",
+        "imgurl":"images/java.jpg",
+
+        
+      },
+      {
+        "name":"Python",
+        "description":"Python Projects",
+        "imgurl":"images/python.jpg"
+        
+      },
+      {
+        "name":"Web Design",
+        "description":"HTML5/CSS3 Projects",  
+        "imgurl":"images/css.jpg"
+      },
+    ],
+    "testimonials":[
+    ]
+  }
+  
+  export default resumeData
